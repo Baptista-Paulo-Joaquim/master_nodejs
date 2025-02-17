83b2d1820dbe39129047290473581abee565aced
Baptista-Paulo-Joaquim <baptistajoaquim0@gmail.com>
2025-02-17 16:11:20 +0200

    Nova branch e arquivo

--- src/tests/soma.js
+++ src/tests/soma.js
@@ -1,6 +1,5 @@
 function soma(a, b){
     return a + b;
 }
-soma(2, 3)
 
 module.exports = soma;
--- src/tests/soma.test.js
+++ src/tests/soma.test.js
@@ -1,15 +1,12 @@
 const soma = require("./soma");
+const multiplicacao = require("./multiplicacao");
 
 describe("Teste de soma", () => {
     it("Soma de 2, 3 deve resultar em 5", () => {
         expect(soma(2, 3)).toBe(5);
     });
 
-    it("Soma de 1, 1 deve resultar em 2", () => {
-        expect(soma(1, 1)).toBe(2);
-    });
-
-    it("Soma de 4, 3 deve resultar em 5", () => {
-        expect(soma(4, 3)).toBe(7);
+    it("Multiplicacao de 4, 3 deve resultar em 12", () => {
+        expect(multiplicacao(2, 3)).toBe(12);
     });
 });
